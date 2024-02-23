ea2126c0e616b9b1222b8c076cbb0dcc1ee0b4be
David Fischer <david@konst.fish>
2024-02-23 20:48:51 +0100

    feat: i don't even know

--- pkg/data/management/machinedriver_data.go
+++ pkg/data/management/machinedriver_data.go
@@ -2,13 +2,11 @@ package management
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"reflect"
 	"strings"
 
 	v32 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
-	"github.com/rancher/rancher/pkg/features"
 	v3 "github.com/rancher/rancher/pkg/generated/norman/management.cattle.io/v3"
 	"github.com/rancher/rancher/pkg/types/config"
 	"github.com/sirupsen/logrus"
@@ -35,7 +33,7 @@ const (
 	OutscaleDriver     = "outscale"
 )
 
-var DriverData = map[string]map[string][]string{
+/*var DriverData = map[string]map[string][]string{
 	Amazonec2driver:    {"publicCredentialFields": []string{"accessKey"}, "privateCredentialFields": []string{"secretKey"}},
 	Azuredriver:        {"publicCredentialFields": []string{"clientId", "subscriptionId", "tenantId", "environment"}, "privateCredentialFields": []string{"clientSecret"}, "optionalCredentialFields": []string{"tenantId"}},
 	DigitalOceandriver: {"privateCredentialFields": []string{"accessToken"}},
@@ -53,6 +51,215 @@ var DriverData = map[string]map[string][]string{
 	Vmwaredriver:       {"publicCredentialFields": []string{"username", "vcenter", "vcenterPort"}, "privateCredentialFields": []string{"password"}},
 	GoogleDriver:       {"privateCredentialFields": []string{"authEncodedJson"}},
 	OutscaleDriver:     {"publicCredentialFields": []string{"accessKey", "region"}, "privateCredentialFields": []string{"secretKey"}},
+}*/
+
+type DriverData struct {
+	Name               string
+	Url                map[string]DriverUrl
+	UiUrl              string
+	Whitelist          []string
+	Active             bool
+	Builtin            bool
+	AddCloudCredential bool
+	Annotations        map[string][]string
+}
+
+type DriverUrl struct {
+	Url      string
+	Checksum string
+}
+
+var driverData = []DriverData{
+	{
+		Name: "pinganyunecs",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "https://drivers.rancher.cn/node-driver-pinganyun/0.3.0/docker-machine-driver-pinganyunecs.tgz",
+				Checksum: "f84ccec11c2c1970d76d30150916933efe8ca49fe4c422c8954fc37f71273bb5",
+			},
+			"arm64": DriverUrl{
+				Url:      "https://drivers.rancher.cn/node-driver-pinganyun/0.3.0/docker-machine-driver-pinganyunecs.tgz",
+				Checksum: "f84ccec11c2c1970d76d30150916933efe8ca49fe4c422c8954fc37f71273bb5",
+			},
+		},
+		UiUrl:              "https://drivers.rancher.cn/node-driver-pinganyun/0.3.0/component.js",
+		Whitelist:          []string{"drivers.rancher.cn"},
+		Active:             false,
+		Builtin:            false,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
+	{
+		Name: "aliyunecs",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "https://drivers.rancher.cn/node-driver-aliyun/1.0.4/docker-machine-driver-aliyunecs.tgz",
+				Checksum: "5990d40d71c421a85563df9caf069466f300cd75723effe4581751b0de9a6a0e",
+			},
+			"arm64": DriverUrl{
+				Url:      "https://drivers.rancher.cn/node-driver-aliyun/1.0.4/docker-machine-driver-aliyunecs.tgz",
+				Checksum: "5990d40d71c421a85563df9caf069466f300cd75723effe4581751b0de9a6a0e",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          []string{"ecs.aliyuncs.com"},
+		Active:             false,
+		Builtin:            false,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
+	{
+		Name: "amazonec2",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+			"arm64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          []string{"iam.amazonaws.com", "iam.us-gov.amazonaws.com", "iam.%.amazonaws.com.cn", "ec2.%.amazonaws.com", "ec2.%.amazonaws.com.cn", "eks.%.amazonaws.com", "eks.%.amazonaws.com.cn", "kms.%.amazonaws.com", "kms.%.amazonaws.com.cn"},
+		Active:             true,
+		Builtin:            true,
+		AddCloudCredential: true,
+		Annotations:        nil,
+	},
+	{
+		Name: "azure",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+			"arm64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          nil,
+		Active:             true,
+		Builtin:            true,
+		AddCloudCredential: true,
+		Annotations:        nil,
+	},
+	{
+		Name: "cloudca",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "https://github.com/cloud-ca/docker-machine-driver-cloudca/files/2446837/docker-machine-driver-cloudca_v2.0.0_linux-amd64.zip",
+				Checksum: "2a55efd6d62d5f7fd27ce877d49596f4",
+			},
+			"arm64": DriverUrl{
+				Url:      "https://github.com/cloud-ca/docker-machine-driver-cloudca/files/2446837/docker-machine-driver-cloudca_v2.0.0_linux-arm64.zip",
+				Checksum: "2a55efd6d62d5f7fd27ce877d49596f4",
+			},
+		},
+		UiUrl:              "https://objects-east.cloud.ca/v1/5ef827605f884961b94881e928e7a250/ui-driver-cloudca/v2.1.2/component.js",
+		Whitelist:          []string{"objects-east.cloud.ca"},
+		Active:             false,
+		Builtin:            false,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
+	{
+		Name: "cloudscale",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "https://github.com/cloudscale-ch/docker-machine-driver-cloudscale/releases/download/v1.2.0/docker-machine-driver-cloudscale_v1.2.0_linux_amd64.tar.gz",
+				Checksum: "e33fbd6c2f87b1c470bcb653cc8aa50baf914a9d641a2f18f86a07c398cfb544",
+			},
+			"arm64": DriverUrl{
+				Url:      "https://github.com/cloudscale-ch/docker-machine-driver-cloudscale/releases/download/v1.2.0/docker-machine-driver-cloudscale_v1.2.0_linux_arm64.tar.gz",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "https://objects.rma.cloudscale.ch/cloudscale-rancher-v2-ui-driver/component.js",
+		Whitelist:          []string{"objects.rma.cloudscale.ch"},
+		Active:             false,
+		Builtin:            false,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
+	{
+		Name: "digitalocean",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+			"arm64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          []string{"api.digitalocean.com"},
+		Active:             true,
+		Builtin:            true,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
+	{
+		Name: "exoscale",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+			"arm64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          []string{"api.exoscale.ch"},
+		Active:             false,
+		Builtin:            true,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
+	{
+		Name: "google",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+			"arm64": DriverUrl{
+				Url:      "local://",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          nil,
+		Active:             false,
+		Builtin:            true,
+		AddCloudCredential: true,
+		Annotations:        nil,
+	},
+	{
+		Name: "harvester",
+		Url: map[string]DriverUrl{
+			"amd64": DriverUrl{
+				Url:      "https://releases.rancher.com/harvester-node-driver/v0.6.5/docker-machine-driver-harvester-amd64.tar.gz",
+				Checksum: "8de48b07dd2e8b7ee60ec99b8456925e9c16a7523affb61a5f1788868bb1f8f6",
+			},
+			"arm64": DriverUrl{
+				Url:      "https://github.com/cloud-ca/docker-machine-driver-cloudca/files/2446837/docker-machine-driver-cloudca_v2.0.0_linux-arm64.zip",
+				Checksum: "",
+			},
+		},
+		UiUrl:              "",
+		Whitelist:          []string{"releases.rancher.com"},
+		Active:             true,
+		Builtin:            true,
+		AddCloudCredential: false,
+		Annotations:        nil,
+	},
 }
 
 var driverDefaults = map[string]map[string]string{
@@ -72,78 +279,21 @@ type machineDriverCompare struct {
 }
 
 func addMachineDrivers(management *config.ManagementContext) error {
-	if err := addMachineDriver("pinganyunecs", "https://drivers.rancher.cn/node-driver-pinganyun/0.3.0/docker-machine-driver-pinganyunecs-linux.tgz", "https://drivers.rancher.cn/node-driver-pinganyun/0.3.0/component.js", "f84ccec11c2c1970d76d30150916933efe8ca49fe4c422c8954fc37f71273bb5", []string{"drivers.rancher.cn"}, false, false, false, management); err != nil {
+	/*var arch = runtime.GOARCH
+
+	for _, driver := range driverData {
+		if err := addMachineDriver(driver.Name, driver.Url[arch], driver.UiUrl, driver.Checksum, driver.Whitelist, driver.Active, driver.Builtin, driver.AddCloudCredential, driver.Annotations, management); err != nil {
+			return err
+		}
+	}*/
+
+	if err := addMachineDriver("hetzner", "https://github.com/JonasProgrammer/docker-machine-driver-hetzner/releases/download/5.0.2/docker-machine-driver-hetzner_5.0.2_linux_arm64.tar.gz", "https://storage.googleapis.com/hcloud-rancher-v2-ui-driver/component.js", "", []string{"storage.googleapis.com"}, true, false, false, nil, management); err != nil {
 		return err
 	}
-	if err := addMachineDriver("aliyunecs", "https://drivers.rancher.cn/node-driver-aliyun/1.0.4/docker-machine-driver-aliyunecs.tgz", "", "5990d40d71c421a85563df9caf069466f300cd75723effe4581751b0de9a6a0e", []string{"ecs.aliyuncs.com"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(Amazonec2driver, "local://", "", "",
-		[]string{"iam.amazonaws.com", "iam.us-gov.amazonaws.com", "iam.%.amazonaws.com.cn", "ec2.%.amazonaws.com", "ec2.%.amazonaws.com.cn", "eks.%.amazonaws.com", "eks.%.amazonaws.com.cn", "kms.%.amazonaws.com", "kms.%.amazonaws.com.cn"},
-		true, true, true, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(Azuredriver, "local://", "", "", nil, true, true, true, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver("cloudca", "https://github.com/cloud-ca/docker-machine-driver-cloudca/files/2446837/docker-machine-driver-cloudca_v2.0.0_linux-amd64.zip", "https://objects-east.cloud.ca/v1/5ef827605f884961b94881e928e7a250/ui-driver-cloudca/v2.1.2/component.js", "2a55efd6d62d5f7fd27ce877d49596f4", []string{"objects-east.cloud.ca"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver("cloudscale", "https://github.com/cloudscale-ch/docker-machine-driver-cloudscale/releases/download/v1.2.0/docker-machine-driver-cloudscale_v1.2.0_linux_amd64.tar.gz", "https://objects.rma.cloudscale.ch/cloudscale-rancher-v2-ui-driver/component.js", "e33fbd6c2f87b1c470bcb653cc8aa50baf914a9d641a2f18f86a07c398cfb544", []string{"objects.rma.cloudscale.ch"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(DigitalOceandriver, "local://", "", "", []string{"api.digitalocean.com"}, true, true, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(ExoscaleDriver, "local://", "", "", []string{"api.exoscale.ch"}, false, true, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(GoogleDriver, "local://", "", "", nil, false, true, true, management); err != nil {
-		return err
-	}
-	harvesterEnabled := features.GetFeatureByName(HarvesterDriver).Enabled()
-	// make sure the version number is consistent with the one at Line 40 of package/Dockerfile
-	if err := addMachineDriver(HarvesterDriver, "https://releases.rancher.com/harvester-node-driver/v0.6.5/docker-machine-driver-harvester-amd64.tar.gz", "", "8de48b07dd2e8b7ee60ec99b8456925e9c16a7523affb61a5f1788868bb1f8f6", []string{"releases.rancher.com"}, harvesterEnabled, harvesterEnabled, false, management); err != nil {
-		return err
-	}
-	linodeBuiltin := true
-	if dl := os.Getenv("CATTLE_DEV_MODE"); dl != "" {
-		linodeBuiltin = isCommandAvailable("docker-machine-driver-linode")
-	}
-	if err := addMachineDriver(Linodedriver, "https://github.com/linode/docker-machine-driver-linode/releases/download/v0.1.11/docker-machine-driver-linode_linux-amd64.zip", "/assets/rancher-ui-driver-linode/component.js", "b31b6a504c59ee758d2dda83029fe4a85b3f5601e22dfa58700a5e6c8f450dc7", []string{"api.linode.com"}, linodeBuiltin, linodeBuiltin, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(OCIDriver, "https://github.com/rancher-plugins/rancher-machine-driver-oci/releases/download/v1.3.0/docker-machine-driver-oci-linux", "", "0a1afa6a0af85ecf3d77cc554960e36e1be5fd12b22b0155717b9289669e4021", []string{"*.oraclecloud.com"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(OpenstackDriver, "local://", "", "", nil, false, true, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(OTCDriver, "https://github.com/rancher-plugins/docker-machine-driver-otc/releases/download/v2019.5.7/docker-machine-driver-otc", "", "3f793ebb0ebd9477b9166ec542f77e25", nil, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(PacketDriver, "https://github.com/equinix/docker-machine-driver-metal/releases/download/v0.6.0/docker-machine-driver-metal_linux-amd64.zip", "https://rancher-drivers.equinixmetal.net/1.0.2/component.js", "fad5e551a35d2ef2db742b07ca6d61bb9c9b574d322d3000f0c557d5fb90a734", []string{"api.packet.net", "api.equinix.com", "rancher-drivers.equinixmetal.net"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(PhoenixNAPDriver, "https://github.com/phoenixnap/docker-machine-driver-pnap/releases/download/v0.4.0/docker-machine-driver-pnap_0.4.0_linux_amd64.zip", "", "0bc81bdc80ab258fa0db67918f3b04435ed2c81f84c942c9123a0729f884190b", []string{"api.securedservers.com", "api.phoenixnap.com", "auth.phoenixnap.com"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(RackspaceDriver, "local://", "", "", nil, false, true, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(SoftLayerDriver, "local://", "", "", nil, false, true, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(NutanixDriver, "https://github.com/nutanix/docker-machine/releases/download/v3.4.0/docker-machine-driver-nutanix", "https://nutanix.github.io/rancher-ui-driver/v3.4.0/component.js", "65dbf92e2df2b4c6d1a6b1f77c542f2cb13a052a62f236eeee697a1151ede62c", []string{"nutanix.github.io"}, false, false, false, management); err != nil {
-		return err
-	}
-	if err := addMachineDriver(OutscaleDriver, "https://github.com/outscale/docker-machine-driver-outscale/releases/download/v0.2.0/docker-machine-driver-outscale_0.2.0_linux_amd64.zip", "https://oos.eu-west-2.outscale.com/rancher-ui-driver-outscale/v0.2.0/component.js", "bb539ed4e2b0f1a1083b29cbdbab59bde3efed0a3145fefc0b2f47026c48bfe0", []string{"oos.eu-west-2.outscale.com"}, false, false, false, management); err != nil {
-		return err
-	}
-	return addMachineDriver(Vmwaredriver, "local://", "", "", nil, true, true, false, management)
+	return nil
 }
 
-func addMachineDriver(name, url, uiURL, checksum string, whitelist []string, active, builtin, addCloudCredential bool, management *config.ManagementContext) error {
+func addMachineDriver(name, url, uiURL, checksum string, whitelist []string, active, builtin, addCloudCredential bool, driverAnnotations map[string][]string, management *config.ManagementContext) error {
 	lister := management.Management.NodeDrivers("").Controller().Lister()
 	cli := management.Management.NodeDrivers("")
 	m, _ := lister.Get("", name)
@@ -154,7 +304,7 @@ func addMachineDriver(name, url, uiURL, checksum string, whitelist []string, act
 			annotations[k] = v
 		}
 	}
-	for key, fields := range DriverData[name] {
+	for key, fields := range driverAnnotations {
 		annotations[key] = strings.Join(fields, ",")
 	}
 	defaults := []string{}
